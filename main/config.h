86006d8fa52afa1b80885e07d7255b08d0849689
Martin Dardis <martindardis96@gmail.com>
2018-08-19 21:49:35 -0300

    config parameter moved to  a header file

---
+++ main/config.h
@@ -0,0 +1,26 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define MAX_LONG_STR 100
+#define OTA_NAME "OUT_CONTROL"
+#define OTA_PASS "mdardis" 
+#define OTA_MD5 "d4d0f48c29fa625f6acc2bf70a353a0b"
+
+bool logged = false; //Init without logged session
+
+const int WAIT_TIME = 40; //Time (in seconds) to wait wifi before enable AP mode
+
+#define GPIO0 D1
+bool gpio0_state=false;
+#define GPIO1 D2
+bool gpio1_state=false;
+
+#define WPA_PATH "/wireless.txt"
+#define AP_SSID "ESP_8266"  
+#define AP_PASS "control"
+
+#define LOG_CONF_PATH "/logging.dat"
+String USERNAME = "admin";//Default username
+String PASSWORD = "admin";//Default password
+
+#endif 
